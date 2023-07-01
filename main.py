f7e21058535d85b8ad492245e9f7e456521cdc22
Deibith1810 <deibithandre1810@gmail.com>
2023-06-30 20:16:54 -0500

    subir todos los archivos

---
+++ main.py
@@ -0,0 +1,6 @@
+def sum(x,y):
+    return x+y
+
+def es_mayor_que(num_1,num_2):
+      return num_1>num_2
+  
---
+++ test_main.py
@@ -0,0 +1,9 @@
+from main import sum, es_mayor_que
+
+def test_sum():
+    assert sum (3,8)==12 
+    
+    if __name__== "__main__":
+          test_sum()
+    
+    
